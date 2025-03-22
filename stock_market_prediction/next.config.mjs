b1992aa3dc74abde5767cadd089780a91a633348
LazyWiz01 <jothieshvaran.thangavel@gmail.com>
2025-03-22 11:35:40 +0530

    HELLO

--- stock_market_prediction/next.config.mjs
+++ stock_market_prediction/next.config.mjs
@@ -9,7 +9,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+<<<<<<< HEAD
   // Removed deprecated swcMinify option
+=======
+  swcMinify: true,
+>>>>>>> 9ac05d5bda54592b020bae5a19e70bad4dff0e16
 };
 
 export default nextConfig;
